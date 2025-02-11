e64423cb3c18abc90434c7322847682bfe653e6a
Hazz-i <wahidh697@gmail.com>
2025-02-11 10:45:26 +0700

    add: file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,25 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
+
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ClerkProvider>
+    <SignedOut>
+      <SignInButton />
+    </SignedOut>
+    <SignedIn>
+      <UserButton />
+    </SignedIn>
+    <Component {...pageProps} />
+  </ClerkProvider>
+  );
 }
